41ed85229cb4eb4c0fc6fa760aef19b0c4802d92
Eldarian <eldarian3@gmail.com>
2021-03-01 18:39:04 +0300

    Fontain and basic pathfinding

---
+++ Assets/MoveEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    NavMeshAgent agent;
+    GameObject fortress;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        fortress = GameObject.FindGameObjectWithTag("Finish");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(fortress.transform.position);
+        if(Vector3.Distance(transform.position, fortress.transform.position) < 7.5f)
+        {
+            agent.velocity = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/MapClickHandler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MapClickHandler : MonoBehaviour
+{
+    [SerializeField] GameObject unitPrefab;
+    [SerializeField] bool placementMode = false;
+    [SerializeField] bool walkMode = true;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            placementMode = !placementMode;
+        }
+    }
+
+    private void OnMouseOver() //Basic mouseover
+    {
+        if (placementMode)
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
+            
+            //print(ray);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log(hit.point);
+
+                Instantiate(unitPrefab, hit.point, Quaternion.identity);
+            }
+        }
+
+        if (walkMode)
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var point = ray.origin + (ray.direction);
+            point.z = 0;
+            if (Input.GetMouseButtonDown(1))
+            {
+                //SendToPoint(point);
+            }
+        }
+
+    }
+
+    private void SendToPoint(Vector3 point)
+    {
+        List<Unit> selectedUnits = FindObjectsOfType<Unit>().Where(unit => unit.isSelected).ToList();
+        int length = selectedUnits.Count;
+        int size = Mathf.RoundToInt(Mathf.Sqrt(length));
+        float offset = -1 * (size-1) * 0.7f;
+
+        List<Vector3> destinations = new List<Vector3>();
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                destinations.Add(new Vector3(i + offset, j + offset, 2));
+            }
+        }
+
+        destinations.ForEach(vector => print(vector));
+        var enumerator = destinations.GetEnumerator();
+        enumerator.MoveNext();
+        selectedUnits.ForEach(unit =>
+        {
+            unit.Move(enumerator.Current+point);
+            enumerator.MoveNext();
+        });
+    }
+}
