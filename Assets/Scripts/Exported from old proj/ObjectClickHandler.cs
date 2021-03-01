b835fdb44e0abffe0e42646beae15f12cd2d0c85
Eldarian <eldarian3@gmail.com>
2021-03-01 15:43:59 +0300

    Geometry primitives inherited by empties

---
+++ Assets/Scripts/Exported from old proj/ObjectClickHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClickHandler : MonoBehaviour
+{
+    SpriteRenderer selection;
+    [SerializeField] SelectableObject objectScript;
+
+    private void Awake()
+    {
+        selection = transform.Find("selection").GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseOver()
+    {
+        //print("over");
+        if (Input.GetMouseButtonDown(0))
+        {
+            objectScript.isSelected = !objectScript.isSelected;
+            selection.enabled = objectScript.isSelected;
+        }
+    }
+}
---
+++ Assets/Scripts/Exported from old proj/Unit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : SelectableObject
+{
+    Vector3 destination;
+    [SerializeField] float speed = 1;
+
+    public void Move(Vector3 destination)
+    {
+        this.destination = destination;
+        print(gameObject.name + " goes to " + destination);
+        StartCoroutine(PerformMovement(destination));
+    }
+
+    IEnumerator PerformMovement(Vector3 destination)
+    {
+        while (Vector3.Distance(transform.position, destination) > Mathf.Epsilon)
+        {
+            if (destination != this.destination)
+            {
+                yield break;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
+            yield return new WaitForFixedUpdate();
+        }
+    } 
+}
